c2ae1562cfddc8d0976e301b32bafef33190f750
Cale Cohee <calecohee@gmail.com>
2018-10-24 14:08:16 -0400

    Corrected path to logging layer id

--- plugins/omar-wfs-plugin/grails-app/services/omar/wfs/WebFeatureService.groovy
+++ plugins/omar-wfs-plugin/grails-app/services/omar/wfs/WebFeatureService.groovy
@@ -420,7 +420,7 @@ class WebFeatureService
               requestMethod: requestMethod, httpStatus: httpStatus, endTime: DateUtil.formatUTC(endTime),
               responseTime: responseTime, responseSize: responseSize, filter: filter, maxFeatures: maxFeatures,
               numberOfFeatures: results?.numberOfFeatures, numberMatched: results?.numberMatched, keyword_countryCode: keyword_countryCode,
-              keyword_missionId: keyword_missionId, keyword_sensorId: keyword_sensorId, params: wfsParams.toString(), results: results)
+              keyword_missionId: keyword_missionId, keyword_sensorId: keyword_sensorId, params: wfsParams.toString(), layer_id: results?.features?.id )
 
       log.info requestInfoLog.toString()
 
