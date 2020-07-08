4a3bd69842ddfa45436d2c8f9308029ee320b9ef
aimee <aimee@localhost.localdomain>
2020-07-08 16:04:49 -0400

    Create basic Cypress test

--- cypress/integration/ApiTesting.js
+++ cypress/integration/ApiTesting.js
@@ -1,11 +1,14 @@
-describe('My First Test', () => {
-    it('clicking "type" navigates to a new url', () => {
-        cy.visit('https://omar.ossim.io/omar-wfs/webjars/swagger-ui/3.24.0/index.html?url=/omar-wfs/apidoc/getDocuments')
+const methods = ['getFeature', 'describeFeatureType', 'getCapabilities']
 
-        cy.contains('getFeature').click()
-        cy.url().should('include', '/default/getFeature')
-        cy.contains('Try it out').click()
-        cy.contains('Execute').click()
-        cy.contains('200').should('include.text', '200')
+describe('Automated tests for the omar-wfs methods', () => {
+    methods.forEach((method) => {
+        it(`Should test ${method}`, () => {
+            cy.visit('https://omar.ossim.io/omar-wfs/webjars/swagger-ui/3.24.0/index.html?url=/omar-wfs/apidoc/getDocuments')
+            cy.contains(method).should('be.visible').click()
+            cy.url().should('include', '/default/' + method)
+            cy.contains('Try it out').click()
+            cy.contains('Execute').click()
+            cy.contains('200').should('include.text', '200')
+        })
     })
 })
