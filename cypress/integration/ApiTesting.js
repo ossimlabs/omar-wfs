71d66b6dec089fafd43793e047616d6df89e1a68
aimee <aimee@localhost.localdomain>
2020-07-08 17:26:36 -0400

    Get methods from Swagger Spec

--- cypress/integration/ApiTesting.js
+++ cypress/integration/ApiTesting.js
@@ -1,6 +1,22 @@
-const methods = ['getFeature', 'describeFeatureType', 'getCapabilities']
+let methods = ['getFeature', 'describeFeatureType', 'getCapabilities']
 
 describe('Automated tests for the omar-wfs methods', () => {
+    before(() => {
+        function readTextFile(file, callback) {
+            const rawFile = new XMLHttpRequest();
+            rawFile.overrideMimeType("application/json");
+            rawFile.open("GET", file, true);
+            rawFile.onreadystatechange = function() {
+                if (rawFile.readyState === 4 && rawFile.status == "200") {
+                    callback(rawFile.responseText);
+                }
+            }
+            rawFile.send(null);
+        }
+        readTextFile("/home/IdeaProjects/omar-wfs/plugins/omar-wfs-plugin/build/swaggerSpec.json", function(text){
+            methods = JSON.parse(text).keys;
+        });
+    })
     methods.forEach((method) => {
         it(`Should test ${method}`, () => {
             cy.visit('https://omar.ossim.io/omar-wfs/webjars/swagger-ui/3.24.0/index.html?url=/omar-wfs/apidoc/getDocuments')
