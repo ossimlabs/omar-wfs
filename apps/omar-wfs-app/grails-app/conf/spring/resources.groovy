b4d20d44d50cf9294f4e60ad4dc651c6280b5658
sbortman <scott.bortman@maxar.com>
2022-06-17 08:08:52 -0400

    hackaton-1

--- apps/omar-wfs-app/grails-app/conf/spring/resources.groovy
+++ apps/omar-wfs-app/grails-app/conf/spring/resources.groovy
@@ -1,10 +1,3 @@
 // Place your Spring DSL code here
-import omar.wfs.GeoscriptClientService
-import omar.wfs.ExportClientProxyService
-
 beans = {
-  geoscriptService(GeoscriptClientService)
-  exportClientService(ExportClientProxyService) {
-    grailsLinkGenerator = ref("grailsLinkGenerator")
-  }
 }
